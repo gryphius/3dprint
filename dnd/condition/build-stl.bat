6ebb800d798b613b568747c8ab2a53d767c94e88
Oli <oli@wgwh.ch>
2020-01-04 08:36:54 +0100

    set advantage to 2

--- dnd/condition/build-stl.bat
+++ dnd/condition/build-stl.bat
@@ -1,4 +1,4 @@
-openscad -D num=3 -D "txt="""Advantage"" -o advantage.stl conditions.scad
+openscad -D num=2 -D "txt="""Advantage"" -o advantage.stl conditions.scad
 openscad -D num=2 -D "txt="""Disadvantage"" -o disadvantage.stl conditions.scad
 
 openscad -D num=3 -D "txt="""Blinded"" -o blinded.stl conditions.scad
