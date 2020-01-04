00c7eecd585aa5829e06e5204356b8e68642f807
Oli Schacher <oli.schacher@anooky.com>
2020-01-04 08:27:09 +0100

    add more conditions

--- dnd/condition/build-stl.bat
+++ dnd/condition/build-stl.bat
@@ -1,2 +1,19 @@
 openscad -D num=3 -D "txt="""Advantage"" -o advantage.stl conditions.scad
 openscad -D num=2 -D "txt="""Disadvantage"" -o disadvantage.stl conditions.scad
+
+openscad -D num=3 -D "txt="""Blinded"" -o blinded.stl conditions.scad
+openscad -D num=3 -D "txt="""Charmed"" -o charmed.stl conditions.scad
+openscad -D num=3 -D "txt="""Deafened"" -o deafened.stl conditions.scad
+openscad -D num=2 -D "txt="""Frightened"" -o frightened.stl conditions.scad
+openscad -D num=2 -D "txt="""Incapacitated"" -o incapacitated.stl conditions.scad
+openscad -D num=2 -D "txt="""Invisible"" -o invisible.stl conditions.scad
+openscad -D num=2 -D "txt="""Paralyzed"" -o paralyzed.stl conditions.scad
+openscad -D num=2 -D "txt="""Petrified"" -o petrified.stl conditions.scad
+openscad -D num=2 -D "txt="""Poisoned"" -o poisoned.stl conditions.scad
+openscad -D num=3 -D "txt="""Prone"" -o prone.stl conditions.scad
+openscad -D num=2 -D "txt="""Restrained"" -o restrained.stl conditions.scad
+openscad -D num=3 -D "txt="""Stunned"" -o stunned.stl conditions.scad
+openscad -D num=2 -D "txt="""Unconscious"" -o unconscious.stl conditions.scad
+
+
+
