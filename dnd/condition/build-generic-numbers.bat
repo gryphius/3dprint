f9761d05c7e6b306c04abb4ac340cfece7153d5b
Oli <oli@wgwh.ch>
2020-02-25 18:23:08 +0100

    add A-J letters for matching dndbeyond encounter tracker

--- dnd/condition/build-generic-numbers.bat
+++ dnd/condition/build-generic-numbers.bat
@@ -12,3 +12,14 @@ openscad -D num=4 -D "txt="""7"" -o 7.stl conditions.scad
 openscad -D num=4 -D "txt="""8"" -o 8.stl conditions.scad
 openscad -D num=4 -D "txt="""9"" -o 9.stl conditions.scad
 openscad -D num=4 -D "txt="""10"" -o 10.stl conditions.scad
+openscad -D num=4 -D "txt="""A"" -o A.stl conditions.scad
+openscad -D num=4 -D "txt="""B"" -o B.stl conditions.scad
+openscad -D num=4 -D "txt="""C"" -o C.stl conditions.scad
+openscad -D num=4 -D "txt="""D"" -o D.stl conditions.scad
+openscad -D num=4 -D "txt="""E"" -o E.stl conditions.scad
+openscad -D num=4 -D "txt="""F"" -o F.stl conditions.scad
+openscad -D num=4 -D "txt="""G"" -o G.stl conditions.scad
+openscad -D num=4 -D "txt="""H"" -o H.stl conditions.scad
+openscad -D num=4 -D "txt="""I"" -o I.stl conditions.scad
+openscad -D num=4 -D "txt="""J"" -o J.stl conditions.scad
+
